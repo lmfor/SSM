56957558054f8327ca6b84d048584b41a9585238
George <S109680@ltisdschools.net>
2024-04-05 15:56:34 -0500

    Created Character Movement Script in Scene1 and also created Animations for jump, idle, and run

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+
+    private void Awake()
+    {
+        //grabs references of rigidbody & animatior
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector2(-1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            jump();
+        }
+
+        //set animator parameters
+        anim.SetBool("run",horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+        
+
+
+    }
+
+
+    private void jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
