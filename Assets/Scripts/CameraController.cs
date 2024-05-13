ef5656f485997ee9c5a8dd6f463b1c1582850949
George <S109680@ltisdschools.net>
2024-05-13 14:35:28 -0500

    Final update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,24 +6,30 @@ public class CameraController : MonoBehaviour
 {
 
 
+    //Room camera
     [SerializeField] private float speed;
-    private float currentPosx;
+    private float currentPosX;
     private Vector3 velocity = Vector3.zero;
+
+    //Follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
 
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosx, transform.position.y, transform.position.z), 
-            ref velocity, speed * Time.deltaTime);
+        //Room camera
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //Follow player
+        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
-
-    public void MoveToNewRoom(Transform _newRoom) 
+    public void MoveToNewRoom(Transform _newRoom)
     {
-
-        currentPosx = _newRoom.position.x;
-
-
+        currentPosX = _newRoom.position.x;
     }
-
-
 }
+
